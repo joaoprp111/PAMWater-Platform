b153c62c48b528ebeb4f803e0c27fb07b81346f9
João Pereira <joaoprp111@gmail.com>
2022-11-21 15:39:23 +0000

    Migration from heroku to render service

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'HELLO FROM PAMWATER BACKEND SERVICE'
+    return 'HELLO FROM RENDER PAMWATER BACKEND SERVICE'
 
 
 mydb = mysql.connector.connect(
